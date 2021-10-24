c7417ce29494eac81a8fddef9e410422a7149176
Amol Gosavi <agosavi12@gmail.com>
2021-10-24 13:22:14 +0530

    update menu item with react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
